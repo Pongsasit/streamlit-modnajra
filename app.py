0521be06ed6acba83c7d5be8920b6ae08ade1c16
Pongsasit Thongpramoon <48168818+Pongsasit@users.noreply.github.com>
2023-09-29 15:36:35 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+
+st.header('day 5')
+st.subheader('st.write')
+
+
+st.write('hello writing text')
+
+st.write(1234)
+
+st.write()
+df = pd.DataFrame( 
+    {
+    'first column': [1,2,3,4],
+    'second column': [10,20,30,40]
+}
+)
+
+st.write(df)
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+df2 = pd.DataFrame(
+    np.random.randn(200,3),
+    columns = ['a', 'b', 'c'])
+
+c = alt.Chart(df2).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+)
+
+st.write(c)
+
+
